80b993eb0869c69ff92455e9d0f516d52ef955f8
chyuan <chyuan@dev.4dq.com>
2017-03-31 16:18:39 +0800

    Doge-知乎日报首页数据加载动画开发及UI调整

--- src/view/home.vue
+++ src/view/home.vue
@@ -1,5 +1,6 @@
 <template>
 	<div>
+		<!--头部轮播组件-->
 		<m-swipe swipeid="swipe" ref="swiper" :autoplay="3000" effect="slide">
 			<div @click="go(top.id)" v-for="top in tops" class="swiper-slide" slot="swiper-con">
 				<img :src="top.image">
@@ -8,6 +9,7 @@
 			</div>
 		</m-swipe>
 		
+		<!--新闻列表-->
 		<div class="newsList" v-for="list in lists">
 			<p class="newsList-time">{{list.date.substring(0,4)+"/"+list.date.substring(4,6)+"/"+list.date.substring(6,8)}}</p>
 			<div class="newsList-con" link @click="go(storie.id)" v-for="storie in list.stories">
@@ -15,6 +17,12 @@
 				<p>{{storie.title}}</p>
 			</div>
 		</div>
+		
+		<!--首次加载动画-->
+		<div class="loading" v-if="!lists.length&&!refreshing">
+			<span class="left"></span>
+		</div>
+		
 		<button @click="loadMore" style="width:100px;height:100px;"></button>
 		<!-- 滑动加载更多组件 -->
 		<!--<div :scroller="scroller" :loading="loading" @load="loadMore"></div>-->
@@ -88,11 +96,13 @@ export default{
 		GetDate(Count) {
 			var dd = new Date();
 			dd.setDate(dd.getDate() + Count); //获取AddDayCount天后的日期
+			
 			var y = dd.getFullYear();
 			var m = dd.getMonth() + 1; //获取当前月份的日期
 			m = m > 10 ? m : "0" + m
 			var d = dd.getDate();
 			d = d >= 10 ? d : "0" + d;
+			console.log('91',y,m,d);
 			return y + "" + m + "" + d;
 		}
 	}
@@ -106,7 +116,7 @@ img{width: 100%;}
 .app-view .swiper-slide{height: 8rem;overflow: hidden;position: relative;}
 .app-view .swiper-container-horizontal >.swiper-pagination-bullets{bottom: 1rem;width: 95%;text-align: right;}
 .app-view .newsList:nth-child(2){margin-bottom: 0;padding-top: 0;}
-.app-view .newsList:nth-child(2) .list-time{top: -.8rem;}
+.app-view .newsList:nth-child(2) .newsList-time{top: -.8rem;}
 
 	
 .swiper-slide div{top: 0;left: 0;width: 100%;height: 100%; opacity: 0.4;position: absolute; background-color: @blue;}
@@ -114,8 +124,19 @@ img{width: 100%;}
 .swiper-slide h3{width: 70%;color: #fff;margin: 0;font-size: 0.5rem;line-height: 1rem;right: 5%;bottom: 2.6rem;text-align: right;position: absolute;text-shadow: 1px 1px 10px rgba(0, 0, 0, .5);}
 
 .newsList{width: 90%;z-index: 1;position: relative;padding-top: 0.8rem;margin: .8rem auto 0;}
-.newsList .newsList-time{top: 0;margin: 0;color: #fff;padding: 0 1rem;font-size: 0.4rem;line-height: 0.8rem;letter-spacing: 0.1rem;border-radius: 0.4rem;text-align: center;background-color: @yellow;transform: translate(0,-50%);position: absolute;box-shadow: 0 3px 10px 0 rgba(91,115,146,0.15);}
+.newsList .newsList-time{top: 0;margin: 0;color: #fff;padding: 0 1rem;font-size: 0.4rem;line-height: 0.8rem;letter-spacing: 0.1rem;border-radius: 0.4rem;text-align: center;background-color:yellow;transform: translate(0,-50%);position: absolute;box-shadow: 0 3px 10px 0 rgba(91,115,146,0.15);}
 .newsList .newsList-con{cursor: pointer;display: flex;padding: 0.3rem;margin-bottom: 0.4rem;border-radius: 0.15rem;align-items: center;background-color: #fff;box-shadow: 0 3px 10px 0 rgba(91,115,146,0.15);}
 .newsList .newsList-con img{width: 2rem;margin-right: 0.4rem;}
-.newsList .newsList-con p{color: @blue;font-size: 0.4rem;text-align: justify;margin: 0;}
+.newsList .newsList-con p{color:#5B7492;font-size: 0.4rem;text-align: justify;margin: 0;}
+
+.loading{width: 25%;height: 0.4rem;margin: 25% auto 0;position: relative;}
+.loading span{display: block;width: 0.4rem;height: 0.4rem;border-radius: 50%;position: absolute;background: @blue;transform: translate(-50%,0);}
+.loading .left{background:yellow;animation: lLeft 2.5s ease infinite;}
+@keyframes lLeft{
+	0%{left: 0;}
+    25%{left: 50%;background: yellow;}
+    50%{left: 100%;background: blue;}
+    75%{left: 50%;background: gray;}
+    100%{left: 0;}
+}
 </style>
